d89409caad20e3179c19470d8d5923177f37e38b
dimassabimanyy <dmsabmny@gmail.com>
2020-10-19 11:35:05 +0700

    +hero +services +experiences +featured-projects section

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,10 @@
 
 module.exports = {
   siteMetadata: {
-    title: "WebDev Portfolio",
-    description: "This is WebDev Portfolio Site",
-    author: "@webdev",
-    twitterUsername: "@john_smilga",
+    title: "Dimas Abimanyu Portfolio",
+    description: "Software Engineer Portfolio",
+    author: "@dimasabimanyy",
+    twitterUsername: "@dimasabimanyy",
     image: "/twitter-img.png",
     siteUrl: "https://testing-strapi-gatsby-build.netlify.app",
   },
@@ -33,8 +33,8 @@ module.exports = {
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [],
-        singleTypes: [],
+        contentTypes: [`jobs`, `projects`],
+        // singleTypes: [],
       },
     },
     // {
